6121405df5615f3d9e82690ca331e87919933e12
invierno123 <61281923+invierno123@users.noreply.github.com>
2020-12-22 17:32:19 +0800

    Create v-on2.vue
    仿造v-on的工作原理

---
+++ v-on2.vue
@@ -0,0 +1,18 @@
+new Vue({
+  directives:{
+    on2:{
+      inserted(el,info){
+        el.addEventListener(info.arg,info.value)
+      },
+      unbind(el,info){
+        el.addEventListener(info.arg,info.value)
+      }
+    }
+  },
+template:`<button v-on2:click='hi'>watch</button>`,
+methods:{
+  hi(){
+    console.log("hey")
+  }
+}
+}).$mount("#app");
